e757ba18518a62021404cace522a695deb6e8819
Josh Cesana <joshuacesana@gmail.com>
2018-05-20 01:37:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,11 @@
 # Add  code here!
 def prime?(num)
   if num < 1 
-    false 
+    return false 
   elsif num <= 3
-    true 
+    return true 
   elsif num % 2 == 0 || num % 3 == 0 
-    false 
-  end 
-  
-  i = 5
-  w = 2
-
-  while i * i <= n
-    if num % i == 0
-      false
-    end 
-    i += w
-    w = 6 - w
+    return false 
   end 
   true
 end 
